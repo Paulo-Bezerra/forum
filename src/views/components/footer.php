7553883f573b06d4405a35d72a8e7ea8978ad5d1
b-andersonlucas <b.andersonlucas@gmail.com>
2020-08-13 13:55:17 -0300

    Algumas marcaçoes foram removidas, nao se faziam necessarias

--- src/views/components/footer.php
+++ src/views/components/footer.php
@@ -1,6 +1,4 @@
-<div id=rodape>
-    <footer>
-            <hr>
-            <p>Forum da fsociedade é propriedade da Melindros&copy; - 2020</p>
-    </footer>
-</div>
+<footer>
+    <hr>
+    <p>Forum da fsociedade é propriedade da Melindros&copy; - 2020</p>
+</footer>
--- src/views/components/header.php
+++ src/views/components/header.php
@@ -1,12 +1,10 @@
-<div id=cabecalho>
-    <header>
-            <h1 id="logo">&lt;FSociedade&gt;</h1>
-            <p id="description">Um local para você falar o que quer, com liberdade. Aproveite!</p>
-            <div class="vertical-menu">
-                <a href="/forum/src/views/index.php">Home</a>
-                <br>
-                <a href="/forum/src/views/new-topic.php">Criar tópico</a>
-            </div>
-            <hr>
-    </header>
-</div>
+<header>
+    <h1 id="logo">&lt;FSociedade&gt;</h1>
+    <p id="description">Um local para você falar o que quer, com liberdade. Aproveite!</p>
+    <div class="vertical-menu">
+        <a href="/forum/src/views/index.php">Home</a>
+        <br>
+        <a href="/forum/src/views/new-topic.php">Criar tópico</a>
+    </div>
+    <hr>
+</header>
